9966d49e2ecf6af2b5a61aa88386df389ee873ab
termyn9 <hrya4kov.ilya@yandex.ru>
2022-11-28 17:00:36 +0300

    CoeffTolerant 70%

--- Diplom/CoeffTolerant.cs
+++ Diplom/CoeffTolerant.cs
@@ -14,7 +14,11 @@ namespace Diplom
     public partial class CoeffTolerant : Form
     {
         CoeffContext db;
+
         int RowCount = 0;
+        double sumX = 0;
+        double sumKAVG = 0;
+
         public CoeffTolerant()
         {
             InitializeComponent();
@@ -37,6 +41,13 @@ namespace Diplom
             dgvExpertMarks.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvDBGroups.AllowUserToAddRows = false;
 
+            double[,] arrayK = new double[dgvExpertMarks.Rows.Count, dgvExpertMarks.ColumnCount];
+            double[] arrayKAVG = new double[dgvExpertMarks.Rows.Count];
+            double[] arrayKprom = new double[dgvExpertMarks.RowCount];
+            double[] arrayKResult = new double[dgvExpertMarks.ColumnCount];
+            double[] arrayVprom = new double[dgvExpertMarks.ColumnCount];
+            double[] arrayV = new double[dgvExpertMarks.RowCount];
+
             for (int i = 1; i <= dgvDBGroups.Rows.Count / 5; i++)
             {
                 dgvExpertMarks.Columns[i].HeaderCell.Value = ($"{i}-ый эксперт").ToString();
@@ -46,6 +57,47 @@ namespace Diplom
                    RowCount++;
                 }
             }
+
+            for (int i = 0; i < dgvExpertMarks.Rows.Count; i++)
+            {
+                for (int j = 0; j < dgvExpertMarks.Columns.Count; j++)
+                {
+                    sumX += Convert.ToDouble(dgvExpertMarks.Rows[i].Cells[j].Value) / dgvExpertMarks.Columns.Count;
+                }
+
+                for (int k = 0; k < dgvExpertMarks.Columns.Count; k++)
+                {
+                    arrayK[i, k] = 1 - Math.Abs(sumX - Convert.ToDouble(dgvExpertMarks.Rows[i].Cells[k].Value)) / 9;
+                }
+
+                sumX = 0;
+
+
+            }
+
+            for (int i = 0; i < dgvExpertMarks.Rows.Count; i++)
+            {
+                for (int j = 0; j < arrayKAVG.Length; j++)
+                {
+                    arrayKprom[i] += arrayK[j, i];
+                }
+                arrayKAVG[i] = arrayKprom[i] / dgvExpertMarks.RowCount;
+
+                sumKAVG += arrayKAVG[i];
+            }
+
+            //for (int i = 0; i < arrayKResult.Length; i++)
+            //{
+            //    arrayKResult[i] = arrayKAVG[i] / sumKAVG;
+            //}
+
+            //for (int i = 0; i < dgvExpertMarks.Columns.Count; i++)
+            //{
+            //    for (int j = 0; j < dgvExpertMarks.Rows.Count; j++)
+            //    {
+            //        arrayVprom[i] += arrayK[i, j] * arrayKResult[i];
+            //    }
+            //}
         }
     }
 }
